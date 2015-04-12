5ce03291b9fd0e93fe1e050059b01fa76a2ac682
James Montemagno <james.montemagno@gmail.com>
2015-04-11 22:58:15 -0700

    Change formatting

--- NView/IView.cs
+++ NView/IView.cs
@@ -2,23 +2,23 @@
 
 namespace NView
 {
-  /// <summary>
-  /// NView View
-  /// </summary>
-  public interface IView
-  {
-    /// <summary>
-    /// Binds the IView to a native view.
-    /// </summary>
-    /// <returns>A disposable view</returns>
-    /// <param name="nativeView">Native view to bind with.</param>
-    IDisposable BindToNative(object nativeView);
+	/// <summary>
+	/// NView View
+	/// </summary>
+	public interface IView
+	{
+		/// <summary>
+		/// Binds the IView to a native view.
+		/// </summary>
+		/// <returns>A disposable view</returns>
+		/// <param name="nativeView">Native view to bind with.</param>
+		IDisposable BindToNative (object nativeView);
 
-    /// <summary>
-    /// Gets the type of the preferred native control.
-    /// </summary>
-    /// <value>The type of the preferred native.</value>
-    Type PreferredNativeType { get; }
-  }
+		/// <summary>
+		/// Gets the type of the preferred native control.
+		/// </summary>
+		/// <value>The type of the preferred native.</value>
+		Type PreferredNativeType { get; }
+	}
 }
 
