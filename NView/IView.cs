adf2b48efdd9c04e06a96716b024b5b06baa8bb8
James Montemagno <james.montemagno@gmail.com>
2015-04-11 22:52:12 -0700

    Initial Commit

---
+++ NView/IView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NView
+{
+  /// <summary>
+  /// NView View
+  /// </summary>
+  public interface IView
+  {
+    /// <summary>
+    /// Binds the IView to a native view.
+    /// </summary>
+    /// <returns>A disposable view</returns>
+    /// <param name="nativeView">Native view to bind with.</param>
+    IDisposable BindToNative(object nativeView);
+
+    /// <summary>
+    /// Gets the type of the preferred native control.
+    /// </summary>
+    /// <value>The type of the preferred native.</value>
+    Type PreferredNativeType { get; }
+  }
+}
+
